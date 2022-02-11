d178e3db2f44abc00b492d5f63b177c554a230d3
Satoshi <8ato2h1@gmail.com>
2022-02-11 07:45:15 +0000

    0%

--- tweet.py
+++ tweet.py
@@ -12,7 +12,9 @@ from twitter import text as txt
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "-test", help="specify y to run a test (this does not post), defaulted no.", type=str, default='n')
+        "-test", help="specify y to run a test (disables posting a tweet), defaulted no.", type=str, default='n')
+    parser.add_argument(
+        "-post", help="specify y to post message, defaulted no.", type=str, default='n')
 
     args = parser.parse_args()
     if args.test == "y":
@@ -26,8 +28,11 @@ def main():
     epoch_start_time = _epoch_latest["start_time"]
     epoch_end_time = _epoch_latest["end_time"]
 
-    unix_time = time.time()
-    # diff = unix_time - epoch_start_time
+    # Set current time or epoch start time in which case postting message will run.
+    if args.post == "y":
+        unix_time = epoch_start_time
+    else:
+        unix_time = time.time()
 
     print(f"Unix time: {unix_time}")
     print(f"Epoch start time: {epoch_start_time}")
@@ -82,7 +87,6 @@ def main():
         pool_obj = Pool()
         p = pool_obj.pool_historical_data(
             calc_epoch)
-
         calc_obj.set_pool_params(p["active_stake"], p["blocks"])
         calc_obj.calculate_params()
 
@@ -93,7 +97,24 @@ def main():
             int(p["active_stake"]) / 1000000 / 1000000, 3)
         # print(f'Active stake: {p["active_stake"]}')
 
-        if performance_in_prct < 50:
+        if performance_in_prct == 0:
+            text = """
+プールのご報告
+申し訳ありません。
+今回ブロックの割り当てはございませんでした。
+
+Epoch {} の成績
+- Blocks minted: {}
+- Active stake: {} M
+
+Epoch {} もサーバ運用はしっかりと実施いたします。
+- Start: {} JST (UTC+9)
+- End  : {} JST (UTC+9)
+
+#Cardano
+        """.format(calc_epoch, p["blocks"], active_stake_in_ada_m, epoch_latest, dt_epoch_start, dt_epoch_end)
+
+        elif performance_in_prct < 50:
             text = """
 プールのご報告
 今回の割り当ては少ない結果でした。
